efcd9101affce0e1354a06a0b3593341ef3bfdde
Francisco Vilches <f_vilches17@outlook.com>
2021-08-28 20:01:08 +1200

    rename blazorserver project to BlazorApp

--- working/templates/blazorserver/Pages/Error.cshtml.cs
+++ working/templates/blazorserver/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BlazorApp1.Pages;
+namespace BlazorApp.Pages;
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
