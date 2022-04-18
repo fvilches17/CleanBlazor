2614b2ed05fac4c0d825a62bb04c9cf3572a02d0
Francisco Vilches <f_vilches17@outlook.com>
2022-04-19 08:14:24 +1200

    add no-https flag support

--- working/templates/blazorserver/Program.cs
+++ working/templates/blazorserver/Program.cs
@@ -11,10 +11,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+#if (RequiresHttps)
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+#else
+}
+
+#endif
 
 app.UseStaticFiles();
 
--- working/templates/blazorwasm/Server/Program.cs
+++ working/templates/blazorwasm/Server/Program.cs
@@ -17,11 +17,16 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
+#if (RequiresHttps)
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+#endif
 }
 
+#if (RequiresHttps)
 app.UseHttpsRedirection();
+
+#endif
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
